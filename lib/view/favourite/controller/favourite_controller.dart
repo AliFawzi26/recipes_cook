15cf105141ce69d635d2b1908b2b7615769d42bc
AliFawzi26 <ah9679412@gmail.com>
2025-06-29 16:10:42 -0700

    Update favourite_controller.dart

--- lib/view/favourite/controller/favourite_controller.dart
+++ lib/view/favourite/controller/favourite_controller.dart
@@ -1,7 +1,19 @@
+
 import 'package:get/get.dart';
+import '../../../Core/local_storge/sqfilte_config.dart';
+import '../../../model/recipec.dart';
+class FavoriteController extends GetxController {
+  final SqfliteConfg db = SqfliteConfg();
+  var favoriteRecipes = <RecipeModel>[].obs;
 
-class FavouriteController extends GetxController{
+  void loadFavoriteRecipes() async {
+    final data = await db.getFavoriteRecipes();
+    favoriteRecipes.assignAll(data);
+  }
 
-
-
+  void toggleFavorite(int id, bool currentValue) async {
+    await db.toggleFavorite(id, !currentValue);
+    loadFavoriteRecipes(); // إعادة التحميل بعد التعديل
+  }
 }
+
