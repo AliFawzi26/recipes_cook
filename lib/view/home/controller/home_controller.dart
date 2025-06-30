b706bff6851c982fd8aae49c3f46abe2ca004353
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:22:29 -0700

    Update home_controller.dart

--- lib/view/home/controller/home_controller.dart
+++ lib/view/home/controller/home_controller.dart
@@ -1,24 +1,23 @@
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../food/sweets/controller/recipes_controller.dart';
-
 class HomeController extends GetxController with GetSingleTickerProviderStateMixin {
   late TabController tabController;
   final searchQuery = ''.obs;
+  final List<String> _tags = ['حلويات', 'سلطات', 'أكلات شرقية', 'أكلات غربية'];
   @override
   void onInit() {
     super.onInit();
     tabController = TabController(length: 4, vsync: this);
   }
-  void filterAllRecipes(String query) {
-    searchQuery.value = query;
 
-    Get.find<RecipesController>(tag: 'حلويات').filterRecipes(query);
-    Get.find<RecipesController>(tag: 'سلطات').filterRecipes(query);
-    Get.find<RecipesController>(tag: 'أكلات شرقية').filterRecipes(query);
-    Get.find<RecipesController>(tag: 'أكلات غربية').filterRecipes(query);
+  void filterCurrentTabRecipes(String query) {
+    searchQuery.value = query;
+    final tag = _tags[tabController.index];
+    Get.find<RecipesController>(tag: tag).filterRecipes(query);
   }
+
   @override
   void onClose() {
     tabController.dispose();
