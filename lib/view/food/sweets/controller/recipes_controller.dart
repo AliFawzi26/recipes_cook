dc502950404dc4f4787e51ae8eafbc92ba055662
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:18:53 -0700

    Update recipes_controller.dart

--- lib/view/food/sweets/controller/recipes_controller.dart
+++ lib/view/food/sweets/controller/recipes_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:get/get.dart';
 import '../../../../Core/local_storge/sqfilte_config.dart';
 import '../../../../model/recipec.dart';
@@ -17,8 +16,6 @@ class RecipesController extends GetxController {
     super.onInit();
     loadFavoriteRecipes();
   }
-
-
   Future<void> loadData() async {
     var data = await db.getRecipesByCategory(category);
     recipesList.assignAll(data);
@@ -26,53 +23,38 @@ class RecipesController extends GetxController {
 
   }
   void filterRecipes(String query){
-
 if(query .isEmpty){
   filteredRecipesList.assignAll(recipesList);
 }else{
   filteredRecipesList.assignAll( recipesList.where((recipe) => recipe.title.contains(query)).toList(),);
-}
-
-
-  }
-
-
+}  }
 
   void addRecipe(RecipeModel recipe) {
     if (recipe.category == category) {
       recipesList.add(recipe);
-    }
-  }
-
-
-
-  // 🟢 تحميل الوصفات المفضل
+    } }
+  //  تحميل الوصفات المفضل
   void loadFavoriteRecipes() async {
     favoriteRecipes.value = await db.getFavoriteRecipes();
   }
-
-  // 🟢 تحديث حالة التفضيل
+  //  تحديث حالة التفضيل
   Future<void> updateFavoriteStatus(RecipeModel recipe) async {
     await db.toggleFavorite(recipe.id!, recipe.isFavorite.value ? true : false);
-
     if (!recipe.isFavorite.value) {
       favoriteRecipes.removeWhere((item) => item.id == recipe.id);
     } else {
       loadFavoriteRecipes();
-    }
-  }
-
-  // 🟢 حذف وصفة
+    }  }
+  //  حذف وصفة
   Future<void> deleteRecipe(int id, String category) async {
     try {
       int result = await db.deleteRecipe(id);
       if (result > 0) {
-        loadData(); // ✅ تحديث القائمة بعد الحذف
+        loadData(); //  تحديث القائمة بعد الحذف
         Get.snackbar("نجاح", "تم حذف المنتج بنجاح", backgroundColor: const Color(0xFF4CAF50));
       } else {
         Get.snackbar("خطأ", "فشل في حذف المنتج", backgroundColor: const Color(0xFFF44336));
       }
     } catch (e) {
       Get.snackbar("خطأ", "حدث خطأ أثناء حذف المنتج: $e", backgroundColor: const Color(0xFFF44336));
-    }
-  }}
+    } }}
