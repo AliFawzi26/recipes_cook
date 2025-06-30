b270ecb0c465a60de6114ce656fe332ce19701cb
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:15:29 -0700

    Update sweets.dart

--- lib/view/food/sweets/screen/sweets.dart
+++ lib/view/food/sweets/screen/sweets.dart
@@ -1,29 +1,29 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
-
 import '../../../../Core/const/app_colors.dart';
 import '../../../widgets/gridview.dart';
 import '../controller/recipes_controller.dart';
-
 class SweetsScreen extends StatelessWidget {
   SweetsScreen({Key? key}) : super(key: key);
-
-  final RecipesController controller = Get.put(RecipesController('حلويات'), tag: 'حلويات');
-
+  final RecipesController controller = Get.put(RecipesController('حلويات'), tag: 'حلويات',);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManger.white,
-      body: Obx(() {
-        if (controller.filteredRecipesList.isEmpty) {
-          return Center(child: Text("لا توجد وصفات مطابقة"));
-        } else {
-          return RecipeGridView(recipes: controller.filteredRecipesList, controller: controller);
-        }
-      }),
+      body: _buildBody(),
     );
+  }
+  Widget _buildBody() {
+    return Obx(() {
+      if (controller.filteredRecipesList.isEmpty) {
+        return const Center(child: Text("لا توجد وصفات مطابقة"));
+      } else {
+        return RecipeGridView(
+          recipes: controller.filteredRecipesList,
+          controller: controller,
+        );
+      }
+    });
   }
 }
