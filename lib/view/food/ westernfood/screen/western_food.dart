0c304e5b9c1a36f4607a8562b44773069a9a793d
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:09:17 -0700

    Update western_food.dart

--- lib/view/food/ westernfood/screen/western_food.dart
+++ lib/view/food/ westernfood/screen/western_food.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:recipes_cook/Core/const/app_colors.dart';
+import '../../../../Core/const/app_colors.dart';
 import '../../../widgets/gridview.dart';
 import '../../sweets/controller/recipes_controller.dart';
+
 class WesternFoodScreen extends StatelessWidget {
   WesternFoodScreen({Key? key}) : super(key: key);
-  final RecipesController controller = Get.put(RecipesController('أكلات غربية'), tag: 'أكلات غربية');
+  final RecipesController controller = Get.put(  RecipesController('أكلات غربية'),  tag: 'أكلات غربية',  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManger.white,
-      body: Obx(() => controller.filteredRecipesList.isEmpty
-          ?  Center(child: Text("لا توجد وصفات بعد"))
-          : RecipeGridView(recipes: controller.filteredRecipesList, controller: controller)),
-
-    );
+      body: _buildBody(),
+    );  }
+  Widget _buildBody() {
+    return Obx(() {
+      if (controller.filteredRecipesList.isEmpty) {
+        return const Center(child: Text("لا توجد وصفات بعد"));
+      } else {
+        return RecipeGridView(  recipes: controller.filteredRecipesList,   controller: controller,  );
+      }
+    });
   }
 }
