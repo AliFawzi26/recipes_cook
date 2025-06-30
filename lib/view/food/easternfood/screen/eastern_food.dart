5dc17114eaeb1d0fedb529e600c16180fab2a812
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:11:15 -0700

    Update eastern_food.dart

--- lib/view/food/easternfood/screen/eastern_food.dart
+++ lib/view/food/easternfood/screen/eastern_food.dart
@@ -1,25 +1,30 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../Core/const/app_colors.dart';
 import '../../../widgets/gridview.dart';
 import '../../sweets/controller/recipes_controller.dart';
 
-
 class EasternFoodScreen extends StatelessWidget {
   EasternFoodScreen({Key? key}) : super(key: key);
-
-  final RecipesController controller = Get.put(RecipesController('أكلات شرقية'), tag: 'أكلات شرقية');
-
+  final RecipesController controller = Get.put(  RecipesController('أكلات شرقية'),  tag: 'أكلات شرقية', );
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: ColorManger.white,
-      body: Obx(() => controller.filteredRecipesList.isEmpty
-          ?  Center(child: Text("لا توجد وصفات بعد"))
-          : RecipeGridView(recipes: controller.filteredRecipesList, controller: controller)),
-
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildBody() {
+    return Obx(() {
+      if (controller.filteredRecipesList.isEmpty) {
+        return const Center(child: Text("لا توجد وصفات بعد"));
+      } else {
+        return RecipeGridView(
+          recipes: controller.filteredRecipesList,
+          controller: controller,
+        );
+      }
+    });
   }
 }
