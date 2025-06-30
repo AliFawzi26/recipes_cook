fae667489c862e2739b2a0fbfa5a8df13c043327
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:23:54 -0700

    Update home_screen.dart

--- lib/view/home/screen/home_screen.dart
+++ lib/view/home/screen/home_screen.dart
@@ -1,65 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:recipes_cook/main.dart';
-import 'package:recipes_cook/view/food/%20westernfood/screen/western_food.dart';
-import 'package:recipes_cook/view/food/easternfood/screen/eastern_food.dart';
-import 'package:recipes_cook/view/food/salads/screen/salads_screen.dart';
-import 'package:recipes_cook/view/food/sweets/screen/sweets.dart';
+import 'package:recipec_food/view/food/easternfood/screen/eastern_food.dart';
+import 'package:recipec_food/view/food/salads/screen/salads_screen.dart';
+import 'package:recipec_food/view/food/sweets/screen/sweets.dart';
 import '../../../Core/const/app_colors.dart';
 import '../../../Core/const/text_style.dart';
 import '../../bottomnav/screen/bottom_nav.dart';
+import '../../food/westernfood/screen/western_food.dart';
 import '../widget/searchbox.dart';
 import '../controller/home_controller.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final HomeController controller = Get.put(HomeController(), permanent: true);
+  static const List<Tab> _tabs = [
+    Tab(child: Text('حلويات', style: FontStyles.tabBar)),
+    Tab(child: Text('سلطات', style: FontStyles.tabBar)),
+    Tab(child: Text('أكلات شرقية', style: FontStyles.tabBar)),
+    Tab(child: Text('أكلات غربية', style: FontStyles.tabBar)),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final HomeController controller = Get.put(HomeController(), permanent: true);
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
+    return Scaffold(
+      backgroundColor: ColorManger.white,
+      appBar: AppBar(
         backgroundColor: ColorManger.white,
-        appBar: AppBar(
-          backgroundColor: ColorManger.white,
-          elevation: 0,
-          title: SearchBox(),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            SizedBox(height: 20.sp),
-            TabBar(
+        elevation: 0,
+        title: SearchBox(),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20.sp),
+          TabBar(
+            controller: controller.tabController,
+            unselectedLabelColor: ColorManger.text2,
+            indicatorColor: ColorManger.oran,
+            labelColor: ColorManger.oran,
+            dividerColor: ColorManger.white,
+            tabs: _tabs,
+          ),
+          SizedBox(height: 20.sp),
+          Expanded(
+            child: TabBarView(
               controller: controller.tabController,
-              unselectedLabelColor: ColorManger.text2,
-              indicatorColor: ColorManger.oran,
-              labelColor: ColorManger.oran,
-              dividerColor: ColorManger.white,
-              tabs: [
-                Tab(child: Text('حلويات', style: FontStyles.tabBar(context))),
-                Tab(child: Text('سلطات', style: FontStyles.tabBar(context))),
-                Tab(child: Text('أكلات شرقية', style: FontStyles.tabBar(context))),
-                Tab(child: Text('أكلات غربية', style: FontStyles.tabBar(context))),
+              children: [
+                SweetsScreen(),
+                SaladsScreen(),
+                EasternFoodScreen(),
+                WesternFoodScreen(),
               ],
             ),
-             SizedBox(height: 20.sp),
-            Expanded(
-              child: TabBarView(
-                controller: controller.tabController,
-                children:  [
-                  SweetsScreen(),
-                  SaladsScreen(),
-                  EasternFoodScreen(),
-                  WesternFoodScreen(),
-                ],
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavBar(),
+          ),
+        ],
       ),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 }
+
