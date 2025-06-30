4f4e78ffb4ae6b1bfb152e401c8497451b97ae48
AliFawzi26 <ah9679412@gmail.com>
2025-06-30 04:13:28 -0700

    Update salads_screen.dart

--- lib/view/food/salads/screen/salads_screen.dart
+++ lib/view/food/salads/screen/salads_screen.dart
@@ -3,22 +3,30 @@ import 'package:get/get.dart';
 import '../../../../Core/const/app_colors.dart';
 import '../../../widgets/gridview.dart';
 import '../../sweets/controller/recipes_controller.dart';
+
 class SaladsScreen extends StatelessWidget {
   SaladsScreen({Key? key}) : super(key: key);
 
-  final RecipesController controller = Get.put(RecipesController('سلطات'), tag: 'سلطات');
-
+  final RecipesController controller = Get.put(RecipesController('سلطات'), tag: 'سلطات',);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManger.white,
-      body: Obx(() {
-        if (controller.filteredRecipesList.isEmpty) {
-          return Center(child: Text("لا توجد وصفات مطابقة"));
-        } else {
-          return RecipeGridView(recipes: controller.filteredRecipesList, controller: controller);
-        }
-      }),
+      body: _buildBody(),
     );
   }
+
+  Widget _buildBody() {
+    return Obx(() {
+      if (controller.filteredRecipesList.isEmpty) {
+        return const Center(child: Text("لا توجد وصفات مطابقة"));
+      } else {
+        return RecipeGridView(
+          recipes: controller.filteredRecipesList,
+          controller: controller,
+        );
+      }
+    });
+  }
 }
+
